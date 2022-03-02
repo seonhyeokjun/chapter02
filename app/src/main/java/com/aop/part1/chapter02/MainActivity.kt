15b359b2a88467c0906f7e93bd2939ebbe646d2f
seonhyeokjun <tjsrmatn@naver.com>
2022-03-03 00:38:05 +0900

    키와 몸무게 입력 화면 기능 구현하기

--- app/src/main/java/com/aop/part1/chapter02/MainActivity.kt
+++ app/src/main/java/com/aop/part1/chapter02/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.aop.part1.chapter02
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val heightEditText: EditText = findViewById(R.id.heightEditText)
+        val weightEditText = findViewById<EditText>(R.id.weightEditText)
+
+        val resultButton = findViewById<Button>(R.id.resultButton)
+
+        resultButton.setOnClickListener {
+            Log.d("MainActivity", "ResultButton 이 클리되었습니다.")
+
+            if (heightEditText.text.isEmpty() || weightEditText.text.isEmpty()) {
+                Toast.makeText(this, "빈 값이 있습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            // 이 아래로는 절대 빈 값이 올 수 없음.
+
+            val height:Int = heightEditText.text.toString().toInt()
+            val weight:Int = weightEditText.text.toString().toInt()
+
+            val intent = Intent(this, ResultActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/aop/part1/chapter02/ResultActivity.kt
@@ -0,0 +1,11 @@
+package com.aop.part1.chapter02
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
